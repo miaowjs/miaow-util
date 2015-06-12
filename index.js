d218810f00cc7dda2a3543463ff637fe2712e643
anhulife <anhulife@gmail.com>
2015-06-12 14:44:35 +0800

    初始提交

---
+++ index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  log: require('./lib/log'),
+  hash: require('./lib/hash'),
+  resolve: require('./lib/resolve'),
+  execPlugins: require('./lib/execPlugins'),
+  getDataURI: require('./lib/getDataURI'),
+  PluginError: require('./lib/PluginError')
+};
